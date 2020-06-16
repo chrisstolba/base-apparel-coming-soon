ac96aae2547a406d45a5ddec6ddf80ddfbac464c
Chris Stolba <chrisstolba@Chriss-MacBook-Pro.local>
2020-06-16 16:44:52 -0700

    First Draft of Project Complete.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import React from 'react';
-import './css/reset.css';
-import './css/main.css';
+import React from "react";
+import { useState } from "react";
+import "./css/reset.css";
+import "./css/main.css";
+
+import CallToAction from "./components/CallToAction";
 
 let App = () => {
+  let [userEmail, setUserEmail] = useState(null);
+  let [inputError, setInputError] = useState(false);
+
+  let handleEmailChange = (evt) => {
+    console.log(evt.target.value);
+    setUserEmail(evt.target.value);
+  };
+
+  let handleEmailSubmit = (evt) => {
+    let emailRE = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (emailRE.test(userEmail)) {
+      setUserEmail(null);
+      evt.target.value = "";
+      setInputError(false);
+    } else {
+      setInputError(true);
+      evt.preventDefault();
+    }
+  };
+
   return (
-    <div className='App'>
-      <h1>solution</h1>
+    <div className="App">
+      <header className="mobileLogoWrapper"></header>
+      <CallToAction
+        handleEmailSubmit={handleEmailSubmit}
+        inputValue={userEmail}
+        handleEmailChange={handleEmailChange}
+        inputError={inputError}
+      />
+      <div className="backgroundImageWrapper"></div>
     </div>
   );
 };
---
+++ src/components/CallToAction.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+let CallToAction = (props) => {
+  let { handleEmailSubmit, handleEmailChange, userEmail, inputError } = props;
+
+  let errorStyle = {
+    border: "1px solid red",
+  };
+
+  let inputErrorMessage = {
+    visibility: "visible",
+  };
+
+  return (
+    <main className="callToActionWrapper">
+      <div className="callToActionContainer">
+        <div className="desktopLogoWrapper"></div>
+        <div className="bannerWrapper">
+          <h1 className="bannerText">{`WE'RE`}</h1>
+          <h1 className="bannerText bold">COMING</h1>
+          <h1 className="bannerText bold">SOON</h1>
+          <p className="bannerDescription">{`Hello fellow shoppers! We're currently building our new fashion store. Add your email below to stay up-to-date with announcements and our launch deals.`}</p>
+        </div>
+        <form className="emailWrapper" onSubmit={handleEmailSubmit}>
+          <input
+            type="text"
+            id="emailInput"
+            name="emailInput"
+            value={userEmail}
+            placeholder="Email Address"
+            onChange={handleEmailChange}
+            style={inputError ? errorStyle : null}
+          />
+          <button type="submit" className="emailSubmitButton">
+            <div className="arrowIcon"></div>
+          </button>
+          <p
+            className="inputError"
+            style={inputError ? inputErrorMessage : null}
+          >
+            Please provide a valid email
+          </p>
+        </form>
+      </div>
+    </main>
+  );
+};
+
+export default CallToAction;
